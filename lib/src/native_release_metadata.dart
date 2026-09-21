934fe36a36236f25541f3fde783a448f45ef06ba
richard <richard@Mac.lan>
2026-09-21 22:48:17 +0100

    Format promoted native release metadata

--- lib/src/native_release_metadata.dart
+++ lib/src/native_release_metadata.dart
@@ -10,35 +10,43 @@ const nativeReleaseArtifacts = <String, NativeReleaseArtifact>{
     archiveName: 'dartaframes-polars-native-0.1.0-aarch64-apple-darwin.tar.gz',
     libraryName: 'libdartaframes_polars_ffi.dylib',
     rawAssetName: 'dartaframes-polars-native-0.1.0-aarch64-apple-darwin.dylib',
-    rawSha256: 'c76dd22844fb64aad76cf28c429f5ec9606993c766a092030cf3ea0e2d54abaa',
+    rawSha256:
+        'c76dd22844fb64aad76cf28c429f5ec9606993c766a092030cf3ea0e2d54abaa',
     rawSize: 132616896,
   ),
   'x86_64-apple-darwin': NativeReleaseArtifact(
     archiveName: 'dartaframes-polars-native-0.1.0-x86_64-apple-darwin.tar.gz',
     libraryName: 'libdartaframes_polars_ffi.dylib',
     rawAssetName: 'dartaframes-polars-native-0.1.0-x86_64-apple-darwin.dylib',
-    rawSha256: '88444528a138cbc23db4e7204dade317b7b611d7dbef77a98de28e64bc20553b',
+    rawSha256:
+        '88444528a138cbc23db4e7204dade317b7b611d7dbef77a98de28e64bc20553b',
     rawSize: 143581256,
   ),
   'aarch64-unknown-linux-gnu': NativeReleaseArtifact(
-    archiveName: 'dartaframes-polars-native-0.1.0-aarch64-unknown-linux-gnu.tar.gz',
+    archiveName:
+        'dartaframes-polars-native-0.1.0-aarch64-unknown-linux-gnu.tar.gz',
     libraryName: 'libdartaframes_polars_ffi.so',
-    rawAssetName: 'dartaframes-polars-native-0.1.0-aarch64-unknown-linux-gnu.so',
-    rawSha256: '8ee2fc021d507d395b45c97790e20261e5db359d2992f4fec08219e4bc25760b',
+    rawAssetName:
+        'dartaframes-polars-native-0.1.0-aarch64-unknown-linux-gnu.so',
+    rawSha256:
+        '8ee2fc021d507d395b45c97790e20261e5db359d2992f4fec08219e4bc25760b',
     rawSize: 141140184,
   ),
   'x86_64-unknown-linux-gnu': NativeReleaseArtifact(
-    archiveName: 'dartaframes-polars-native-0.1.0-x86_64-unknown-linux-gnu.tar.gz',
+    archiveName:
+        'dartaframes-polars-native-0.1.0-x86_64-unknown-linux-gnu.tar.gz',
     libraryName: 'libdartaframes_polars_ffi.so',
     rawAssetName: 'dartaframes-polars-native-0.1.0-x86_64-unknown-linux-gnu.so',
-    rawSha256: '1c8c15849f6df1c4c689adc1b25b5cf5116b1ddf0c7a5e4c709953aa6b5ca4e5',
+    rawSha256:
+        '1c8c15849f6df1c4c689adc1b25b5cf5116b1ddf0c7a5e4c709953aa6b5ca4e5',
     rawSize: 152517320,
   ),
   'x86_64-pc-windows-msvc': NativeReleaseArtifact(
     archiveName: 'dartaframes-polars-native-0.1.0-x86_64-pc-windows-msvc.zip',
     libraryName: 'dartaframes_polars_ffi.dll',
     rawAssetName: 'dartaframes-polars-native-0.1.0-x86_64-pc-windows-msvc.dll',
-    rawSha256: 'ddfb650eaf27829615e2454bb42330c2c1a6874de9956461b0b2a121bd13781f',
+    rawSha256:
+        'ddfb650eaf27829615e2454bb42330c2c1a6874de9956461b0b2a121bd13781f',
     rawSize: 125227008,
   ),
 };
